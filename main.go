54f5afba1d2283507ec3519d36695582c2076da5
Ryan <ryannitz@amazon.com>
2018-10-11 13:38:15 -0400

    Change the text

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ func main() {
 
 		w.WriteHeader(http.StatusOK)
 
-		io.WriteString(w, "Hello World\n")
+		io.WriteString(w, "Hello World - now we have CI/CD\n")
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
